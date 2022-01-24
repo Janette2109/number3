f815c95c65d76ac2e9be0a4032a12d8c66dd3d6b
JannaZZZ <w-e_like@mail.ru>
2022-01-24 17:19:54 +0400

    esfdtrd

--- src/Loader.java
+++ src/Loader.java
@@ -1,7 +1,7 @@
 public class Loader {
     public static void main(String[] args) {
         int i = 45;
-        while (i < 350){
+        while (i < 360){
             i++;
         }
             System.out.println("Value i = " + i);
